479b6ead1b09467ddf177907b6744fd587e2d1f1
gxiaomi <gxiaomi@yeah.net>
2015-12-28 18:59:04 +0800

    添加

---
+++ ZhengLT_iOS/Migoo_ZhengLT_iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Migoo_ZhengLT_iOS
+//
+//  Created by 米果科技 on 15/12/28.
+//  Copyright © 2015年 Migoo Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
